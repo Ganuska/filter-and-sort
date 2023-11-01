51bb2aeb1ada32e37873d7427ea2eb5e924af890
Zoran-Santavec <zoran.santavec@playhera.com>
2023-11-01 21:49:19 +0100

    ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'test-task',
+  base: '/test-task/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
